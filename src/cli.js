07c6db6c745bc2de8565216b1479c511bf438e5e
Tracktor <tracktor@bk.ru>
2022-11-25 15:47:49 +0300

    New changes

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-	const nameless = 'Nameless';
+	const nameless = 'Player';
 	const getUserName = readlineSync.question(`What is your name?: `) || nameless;
 	console.log(`Hello, ${getUserName}!`);
+	return getUserName;
 };
 
 export default greetings;
