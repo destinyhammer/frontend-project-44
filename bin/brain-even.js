603e2b70be9fa8fac20e2b0e8ca9c10995aaf636
Tracktor <tracktor@bk.ru>
2022-11-28 12:46:44 +0300

    Modify bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,12 +6,13 @@ import greetings from '../src/cli.js';
 const brainEven = (playername = 'Player') => {
 	const getRandomNumber = () => Math.round(Math.random() * 100);
 
-	const isEven = (number, answer) => {
-		const lowerAnswer = answer.trim().toLowerCase();
-		if (number % 2 === 0) {
-			return lowerAnswer === 'yes' ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was 'yes'.`;
+	const isEven = (number) => number % 2 === 0;
+
+	const checkAnswer = (number, answer) => {
+		if (isEven(number)) {
+			return answer.toLowerCase() === 'yes' ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was 'yes'.`;
 		} else {
-			return lowerAnswer === 'no' ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was 'no'.`;
+			return answer.toLowerCase() === 'no' ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was 'no'.`;
 		}
 	};
 	
@@ -20,8 +21,8 @@ const brainEven = (playername = 'Player') => {
 	for (let i = 1; i <= 3; i += 1) {
 		const rand = getRandomNumber();
 		console.log('Question:', rand);
-		const answer= readlineSync.question(`Your answer: `);
-		const out = isEven(rand, answer);
+		const answer = readlineSync.question(`Your answer: `);
+		const out = checkAnswer(rand, answer);		
 		console.log(out);
 		if (out.indexOf('wrong') !== -1) {
 			console.log(`Let's try again, ${playername}!`);
