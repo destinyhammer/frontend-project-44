a0d2778545f43b2e9f04898106ed1be549a3f989
Tracktor <tracktor@bk.ru>
2022-11-30 09:11:01 +0300

    Add bin/brain-prime.js Modify Makefile, package.json

---
+++ bin/brain-prime.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { greetings, getRandomInt, startGame } from '../src/index.js';
+
+const brainPrime = () => {
+  const gameHeader = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const isPrime = (number) => {
+    if (number < 1) {
+      return false;
+    }
+    for (let i = 2; i < number; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const askQuestion = () => {
+    const number = getRandomInt(2, 3571);
+
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ').trim();
+    const rightAnswer = isPrime(number) ? 'yes' : 'no';
+
+    if (answer === rightAnswer) {
+      return 'Correct!';
+    }
+    return `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`;
+  };
+
+  const playerName = greetings();
+
+  startGame(askQuestion, playerName, gameHeader, 3);
+};
+
+brainPrime();
+
+export default brainPrime;
